1b86c54171292751bb195d6f01019fcdbed89a08
nick <n.m.98@gmx.de>
2020-02-19 15:05:22 +0100

    first_commit

---
+++ cgi-bin/greeting.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+
+import cgi
+
+
+
+print("""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hello!</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- import the webpage's stylesheet -->
+    <link rel="stylesheet" href="/style.css">
+    
+    <!-- import the webpage's javascript file -->
+    <script src="/script.js" defer></script>
+  </head>  
+  <body>
+    
+    <h1>Hello, python!</h1>
+    
+    </body>
+</html>
+""")
